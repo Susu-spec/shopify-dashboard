b4993b0f6e75ea49cf5e7f8445affedfa3fe1f51
Susu-spec <suleimansuwaibat@gmail.com>
2023-11-25 18:17:08 +0100

    Fix: Button mix-up at main__section--two

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-var up;
-var down;
-
-up = 
-down = document.getElementById("button__down");
-
 
 function hideSection () {
     document.getElementById("extend").style.display = "none";
@@ -12,13 +6,13 @@ function hideSection () {
  function hideOptions () {
     document.getElementById("options").style.display = "none";
     document.getElementById("button__up").style.display = "block";
-    down.style.display = "none";
+    document.getElementById("button__down").style.display = "none";
     console.log("done");
 }
 
 function showOptions () {
     document.getElementById("options").style.display = "block";
-    down.style.display = "block";
-    up.style.display = "none";
+    document.getElementById("button__up").style.display = "none";
+    document.getElementById("button__down").style.display = "block";
     console.log("show done");
 }
