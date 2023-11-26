80494ca4f358989ce395b52f73c448a04fe20679
Susu-spec <suleimansuwaibat@gmail.com>
2023-11-26 04:07:24 +0100

    Update: Remove console.log

--- app.js
+++ app.js
@@ -7,12 +7,10 @@ function hideSection () {
     document.getElementById("options").style.display = "none";
     document.getElementById("button__up").style.display = "block";
     document.getElementById("button__down").style.display = "none";
-    console.log("done");
 }
 
 function showOptions () {
     document.getElementById("options").style.display = "block";
     document.getElementById("button__up").style.display = "none";
     document.getElementById("button__down").style.display = "block";
-    console.log("show done");
 }
