2605ff2d7ad3c4e7bc484c3944e4d83a143c513e
Susu-spec <suleimansuwaibat@gmail.com>
2023-11-27 16:41:04 +0100

    Add: handleClick() function

--- app.js
+++ app.js
@@ -14,3 +14,16 @@ function showOptions () {
     document.getElementById("button__up").style.display = "none";
     document.getElementById("button__down").style.display = "block";
 }
+var event = document.getElementsByClassName("main__three--p");
+
+function handleClick(event) {
+    // Hide all child divs in all parent divs
+    var allChildDivs = document.querySelectorAll('.main__three--p .main__three--sub');
+    for (var i = 0; i < allChildDivs.length; i++) {
+      allChildDivs[i].style.display = "none";
+    }
+
+    // Show the child div inside the clicked parent div
+    var clickedChildDiv = event.currentTarget.querySelector('.main__three--sub');
+    clickedChildDiv.style.display = "block";
+  }
