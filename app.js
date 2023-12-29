e2e3cf8ff1d5b72e2c53c74098e43cbb54b3542d
Susu-spec <suleimansuwaibat@gmail.com>
2023-12-29 12:52:09 +0100

    Add: JS functions

--- app.js
+++ app.js
@@ -5,9 +5,13 @@ const text = document.querySelector(".nav__bar--text"),
       bell = document.querySelector(".bell"),
       notifBox = document.querySelector(".notif"),
       targetInput = document.getElementById("Search"),
-      mainTwo = document.querySelector(".main__section--two");
+      mainTwo = document.querySelector(".main__section--two"),
       mainThree = document.querySelector(".main__three");
 var strNum = document.getElementById("number").innerHTML,
+    options = document.getElementById("options"),
+    down = document.getElementById("button__down"),
+    up = document.getElementById("button__up"),
+    extend = document.getElementById("extend"),
     number = parseInt(strNum),
     segment = document.querySelector(".segment"),
     autoComplete = document.getElementById("auto-complete"),
@@ -19,8 +23,146 @@ var strNum = document.getElementById("number").innerHTML,
     matches = [],
     searchResults = ['Bag', 'Cart', 'Centipede', 'Mizu', 'Akemi', 'Teigen', 'Rendezvous', 'Kareem', 'Rick', 'Morty']; 
 
+//DOCUMENT SET FUNCTIONS//
 targetInput.focus();
+docHeight = Math.max(body.scrollHeight, body.offsetHeight, docElem.clientHeight, docElem.scrollHeight, docElem.offsetHeight);
+if (docHeight != undefined) {
+  offset = docHeight / 4;
+}
 
+//window equals browser tab
+window.addEventListener("scroll", function(event) {
+  scrollPos = body.scrollTop || docElem.scrollTop; //scrolltop = distance from element's top to its topmost visible content
+  console.log(scrollPos);
+
+  (scrollPos > offset ) ? btn.classList.add("visible") : btn.classList.remove("visible");
+
+});
+
+//setTimeout() => delay
+
+/**
+ *  window.scrollTo({ top: 0, behavior: 'smooth' }));
+ */
+
+//EVENT LISTENERS
+btn.addEventListener("click", () => {
+    body.scrollTop = 0;
+    docElem.scrollTop = 0;
+});
+
+text.addEventListener("blur", () => {
+  navList.classList.remove("nav--visible");
+})
+
+text.addEventListener("click", () => {
+  navList.classList.toggle("nav--visible");
+  grow(navList);
+});
+
+bell.addEventListener("blur", () => {
+  notifBox.classList.remove("notif--visible");
+});
+
+bell.addEventListener("click", () => {
+  notifBox.classList.toggle("notif--visible");
+  grow(notifBox);
+});
+
+
+//FUNCTION DECLARATIONS//
+function hide(element) {
+  element.style.display = "none";
+}
+
+function show(element) {
+  element.style.display = "block";
+}
+
+function grow(element) {
+  element.classList.add("growAnimation");
+}
+  
+function hideSection() {
+    hide(extend);
+}
+
+function hideOptions() {
+    hide(options);
+    hide(down);
+    show(up);
+    mainTwo.classList.remove("border--up");
+    mainThree.classList.remove("border--down"); 
+}
+
+function showOptions () {
+    show(options);
+    grow(options);
+    hide(up);
+    show(down);
+    mainTwo.classList.add("border--up");
+    mainThree.classList.add("border--down");
+}
+
+grids.forEach((grid) => {
+  grid.addEventListener("click", function(event) {
+    var icon = grid.querySelector(".icon");
+        button = grid.querySelector(".cust__button");
+        clickedChildDiv = event.currentTarget.querySelector('.main__three--sub');
+        allChildDivs = document.querySelector('.main__three--p .main__three--sub'); 
+
+    document.getElementById("number").innerHTML = number;
+    segment.style.width = number + "rem";
+
+    if (!(icon.classList.contains("svg__rollout"))) {
+      for (var i = 0; i < allChildDivs.length; i++) {
+        allChildDivs[i].style.display = "none";
+      }
+    }
+    show(clickedChildDiv);
+
+    if (event.target === icon) {
+      if (clickedChildDiv.style.display === "block") {
+        hide(clickedChildDiv);
+      }
+    }
+    else {
+      show(clickedChildDiv);
+      grow(grid);
+    }
+
+    button.addEventListener("click", () => {
+      if (!icon.classList.contains("svg__rollout") && number >= 0) {
+        icon.classList.add("svg__rollout");
+        number++;    
+      }
+    });
+
+    icon.addEventListener("click", () => {
+      if (icon.classList.contains("svg__rollout")) {
+        icon.classList.remove("svg__rollout");
+        number--;
+        if (number < 0) {
+          number = 0;
+        }
+      }
+    });
+    if ((icon.classList.contains("svg__rollout")) && (clickedChildDiv.style.display === "block")) {
+      if (clickedChildDiv.style.display === "block") {
+        hide(clickedChildDiv);
+      }
+      var el = grid.nextElementSibling;
+      el.querySelector('.main__three--sub').style.display = "block";
+      grow(el); 
+    }
+  });
+});
+
+
+
+/**
+ * if action passed into toggle function is hide 
+ */
 /**
  * function toggleResults(action) {
   if (action = "show") {
@@ -79,112 +221,5 @@ function displayMatches(matchList) {
 }
 **/
 
-docHeight = Math.max(body.scrollHeight, body.offsetHeight, docElem.clientHeight, docElem.scrollHeight, docElem.offsetHeight);
-
-
-if (docHeight != undefined) {
-  offset = docHeight / 4;
-}
-
-//window equals browser tab
-window.addEventListener("scroll", function(event) {
-  scrollPos = body.scrollTop || docElem.scrollTop; //scrolltop = total number of pixels element is scrolled vertically - distance from element's top to its topmost visible content
-  console.log(scrollPos);
-
-  (scrollPos > offset ) ? btn.classList.add("visible") : btn.classList.remove("visible");
-
-});
-
-//setTimeout() => delay
-/**
- *  window.scrollTo({ top: 0, behavior: 'smooth' }));
- */
-
-btn.addEventListener("click",() => {
-    body.scrollTop = 0;
-    docElem.scrollTop = 0;
-});
-
-text.addEventListener("blur", () => {
-  navList.classList.remove("nav--visible"); 
-})
-
-text.addEventListener("click", () => {
-  navList.classList.toggle("nav--visible");
-});
-
-bell.addEventListener("blur", () => {
-  notifBox.classList.remove("notif--visible");
-  console.log("oop");
-});
-
-bell.addEventListener("click", () => {
-  notifBox.classList.toggle("notif--visible");
-});
-  
-function hideSection () {
-    document.getElementById("extend").style.display = "none";
-  }
-
- function hideOptions () {
-    document.getElementById("options").style.display = "none";
-    document.getElementById("button__up").style.display = "block";
-    document.getElementById("button__down").style.display = "none";
-    mainTwo.classList.remove("border--up");
-    mainThree.classList.remove("border--down"); 
-}
-
-/**
- * if button is clicked and button down is block set button up to block.... and remove ....
- * else if button up is clicked, do the opposite
- */
-
-function showOptions () {
-    document.getElementById("options").style.display = "block";
-    document.getElementById("button__up").style.display = "none";
-    document.getElementById("button__down").style.display = "block";
-    mainTwo.classList.add("border--up");
-    mainThree.classList.add("border--down");
-}
-
-grids.forEach((grid) => {
-  grid.addEventListener("click", function(event) {
-    var icon = grid.querySelector(".icon");
-    var button = grid.querySelector(".cust__button"); 
-
-    button.addEventListener("click", () => {
-      if (!icon.classList.contains("svg__rollout") && number >= 0) {
-        icon.classList.add("svg__rollout");
-        number++;
-      }
-    });
-
-    icon.addEventListener("click", () => {
-      if (icon.classList.contains("svg__rollout")) {
-        icon.classList.remove("svg__rollout");
-        number--;
-        if (number < 0) {
-          number = 0;
-        }
-      }
-    });
-    document.getElementById("number").innerHTML = number;
-    segment.style.width = number + "rem";
-
-    var allChildDivs = document.querySelectorAll('.main__three--p .main__three--sub');
-    for (var i = 0; i < allChildDivs.length; i++) {
-      allChildDivs[i].style.display = "none";
-    }
-    var clickedChildDiv = event.currentTarget.querySelector('.main__three--sub');
-    clickedChildDiv.style.display = "block";
-  });
-});
-
-/**
- * if action passed into toggle function is hide 
- */
-
-
-/**TODO: Add smooth scroll animation */
 
 
